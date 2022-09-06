680a6cf13dd0d95dbf82f701e0df3de9e3b40848
chae-hwa <ihj0519@naver.com>
2022-09-07 00:39:38 +0900

    MemberController 구현 오류 수정

--- src/main/java/com/ll/exam/app10/member/entity/Member.java
+++ src/main/java/com/ll/exam/app10/member/entity/Member.java
@@ -6,18 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-@RequestMapping("/member")
+
 public class Member {
 
-    @GetMapping("/join")
-    public String showJoin(){
-        return "member/join";
-    }
 
-    @PostMapping("/join")
-    @ResponseBody
-    public String join(){
-        return "가입 완료";
-    }
 }
