65b9286f4561f9c8069290c4576ee595c79e684f
chae-hwa <ihj0519@naver.com>
2022-09-15 01:55:29 +0900

    프로필페이지에서 이미지 얻기위해 DB select할 필요 없음

--- src/main/java/com/ll/exam/app10/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app10/member/controller/MemberController.java
@@ -2,8 +2,10 @@ package com.ll.exam.app10.member.controller;
 
 import com.ll.exam.app10.member.entity.Member;
 import com.ll.exam.app10.member.service.MemberService;
+import com.ll.exam.app10.security.dto.MemberContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -63,10 +65,8 @@ public class MemberController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/profile")
-    public String showProfile(Principal principal, Model model){
-        Member loginedMember = memberService.getMemberByUsername(principal.getName());
-
-        model.addAttribute("loginedMember", loginedMember);
+    public String showProfile(@AuthenticationPrincipal MemberContext memberContext, Model model) {
+        model.addAttribute("memberContext", memberContext);
 
         return "member/profile";
     }
