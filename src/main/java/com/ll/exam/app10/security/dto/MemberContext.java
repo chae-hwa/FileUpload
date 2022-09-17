e170abc17796896e42f5ddf2c0c941a216f63137
chae-hwa <ihj0519@naver.com>
2022-09-17 22:56:15 +0900

    캐시를 지우지 않아도 변경된 이미지 바로 보이게

--- src/main/java/com/ll/exam/app10/security/dto/MemberContext.java
+++ src/main/java/com/ll/exam/app10/security/dto/MemberContext.java
@@ -9,6 +9,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 
@@ -47,5 +48,9 @@ public class MemberContext extends User implements OAuth2User{
     @Override
     public String getName() {
         return this.getAttribute(this.userNameAttributeName).toString();
+    }
+
+    public String getProfileImgRedirectUrl() {
+        return "/member/profile/img/" + getId() + "?random=" + UUID.randomUUID();
     }
 }
